50df33201547616639c1666719627ac59c10d5e4
dayoola <ayodayo1@gmail.com>
2020-05-28 21:40:34 +0100

    initialcommit

---
+++ src/main/java/com/example/springintializrstarter/SpringintializrstarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springintializrstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringintializrstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringintializrstarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springintializrstarter/SpringintializrstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springintializrstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringintializrstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
