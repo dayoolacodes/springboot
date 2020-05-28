d72822f93c3869cd9aaaf044f632574c8288011e
dayoola <ayodayo1@gmail.com>
2020-05-28 21:35:05 +0100

    first commit

---
+++ src/main/java/com/example/demo/SpringStarterUsingIdeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterUsingIdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterUsingIdeApplication.class, args);
+	}
+
+}
